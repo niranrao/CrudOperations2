d56e4fec383eee5b46979334e4f1643c2d89cce8
niranrao <108452636+niranrao@users.noreply.github.com>
2023-05-03 20:33:04 +0530

    Delete src/main/java/com/lumen/ebonding directory

--- src/main/java/com/lumen/ebonding/service/CustomerService.java
+++
@@ -1,8 +0,0 @@
-package com.lumen.ebonding.service;
-
-import com.lumen.ebonding.dto.CustomerDTO;
-import com.lumen.ebonding.model.Customer;
-
-public interface CustomerService extends GenericService<Customer, Integer, CustomerDTO>{
-
-}
